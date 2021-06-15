e20ef2ff5a0c36417844ad550d01f10cc4cc6826
John N. Norman <normanj777@gmail.com>
2021-06-15 13:29:35 -0400

    add interactive mobile menue

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 const burger = document.getElementById('burger')
 const nav = document.getElementById('nav')
 
-// nav.classList.add('active')
-
 burger.addEventListener('click', e => {
     nav.classList.toggle('active')
 })
